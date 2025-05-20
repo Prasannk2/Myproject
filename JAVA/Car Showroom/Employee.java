81ad7d91e0b44125397c347fc0550f8b6e2de4f4
Prasannk2 <prasannkumar2k91@gmail.com>
2025-05-20 14:20:22 +0530

    change location

---
+++ JAVA/Car Showroom/Employee.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.UUID;
+
+public class Employee extends Showroom implements Utility {
+    String empId;
+    String empName;
+    String empDepartment;
+    int empAge;
+
+    public void getDetails(){
+        System.out.println("=====================================");
+        System.out.println("Employee Id: " + empId);
+        System.out.println("Employee Name: "+ empName);
+        System.out.println("Employee Age: "+ empAge);
+        System.out.println("Employee Department Id: "+ empDepartment);
+        System.out.println("Showroom name: "+ showroomName);
+        System.out.println("=====================================");
+    }
+    public void setDetails(){
+        Scanner e = new Scanner(System.in);
+        UUID vvid = UUID.randomUUID();
+        empId = String.valueOf(vvid);
+        System.out.print("Enter Employee Department name: ");
+        empDepartment = e.nextLine();
+        System.out.print("Enter Employee Name: ");
+        empName = e.nextLine();
+        System.out.print("Enter Employee Age: ");
+        empAge = e.nextInt();
+        System.out.print("The Employee Id is: " + empId);
+        System.out.print("sHOWROOM NAME: ");
+        showroomName = e.nextLine();
+        // totalEmp++;
+       
+    }
+}
